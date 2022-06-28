9dab33b12445ea609b1b61c8468da9ed95f49089
AbijaK <abijamk@gmail.com>
2022-06-28 14:42:59 +0200

    script: show message of error

--- js/script.js
+++ js/script.js
@@ -10,7 +10,16 @@ const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
     // prevent the form from submitting
     e.preventDefault();
-    console.log(isRequired(usernameElt));
+
+    checkUsername();
+
+
+    // console.log(isRequired(usernameElt));
+    // const l = usernameElt.value.length
+    // if (!isBetween(l, 3, 25)) {
+    //     showError(usernameElt, "Error")
+    // }
+    // console.log();
 })
 
 // develop utility functions
@@ -18,4 +27,24 @@ const isRequired = value => value === "" ? false : true;
 const isBetween = (length, min, max) => length<min || length>max ? false : true;
 
 // develop functions that show the error
-const showError = (input, message) => 
+const showError = (input, message) => {
+    const error = document.querySelector('small');
+    error.textContent = message;
+}
+
+const checkUsername = () => {
+    let valid = false
+    const min = 3
+    const max = 25
+    const username = usernameElt.value.trim();
+
+    if (!isRequired(username)) {
+        showError(usernameElt, 'Le nom d\'utilisateur ne peut pas être vide.')
+    }else if (!isBetween(username.length, min, max)) {
+        showError(usernameElt, `Le nom d\'utilisateur doit être compris entre ${min} et ${max} caractères.`)
+    }else {
+        // @todo : create showSuccess() function to display success message
+        valid = true
+    }
+    return valid
+}
